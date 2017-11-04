26dbb906936d8d12671950febd5cb621bf7ff59e
fain704 <fain704@users.noreply.github.com>
2017-11-04 15:55:05 -0400

    fixed new button bug

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-var animalNames = ["Cat",
+
+
+//display button for each index of the array
+$("document").ready(function(event){
+	var animalNames = ["Cat",
 					"Dog",
 					"Elephant", 
 					"Donkey", 
@@ -9,120 +13,118 @@ var animalNames = ["Cat",
 					"Unicorn",
 					"Shark",
 					"Alien" ];
-var animalSelect;
+	var animalSelect;
 
-function fillButtons(){
+	function fillButtons(){
 
-	$("#animal-buttons").empty();
+		$("#animal-buttons").empty();
 
-	for (let i = 0; i < animalNames.length; i++) {
-		let a = $('<button>')
-		// a.attr("id",animalNames[i]);
-		a.text(animalNames[i]);
-		a.addClass("btnAnimal btn-secondary btn-space");
-		$("#animal-buttons").append(a);
-	}
-};
+		for (let i = 0; i < animalNames.length; i++) {
+			let a = $('<button>');
+			a.attr("id",animalNames[i]);
+			a.text(animalNames[i]);
+			a.addClass("btnAnimal btn-secondary btn-space");
+			$("#animal-buttons").append(a);
+			a.on("click", function(){
+				animalSelect = $(this).text();
+				console.log(this);
+				ajaxCall();
+			});	
+		}
+	};
 
-function addForm(){
-	let form = $("<form>");
-	let inputText = $("<input>");
-	inputText.attr("type", "text");
-	inputText.attr("name", "animalName");
-	inputText.attr("id", "animalValue")
+	function addForm(){
+		let form = $("<form>");
+		let inputText = $("<input>");
+		inputText.attr("type", "text");
+		inputText.attr("name", "animalName");
+		inputText.attr("id", "animalValue")
 
-	let inputBtn =$("<button>");
-	inputBtn.attr("type","button");
-	inputBtn.attr("id","submit");
-	inputBtn.addClass("btn-space btn-primary");
-	inputBtn.text("Submit");
+		let inputBtn =$("<button>");
+		inputBtn.attr("type","button");
+		inputBtn.attr("id","submit");
+		inputBtn.addClass("btn-space btn-primary");
+		inputBtn.text("Submit");
 
-	form.append(inputText);
-	form.append(inputBtn);
+		form.append(inputText);
+		form.append(inputBtn);
 
-	$('#animal-form').append(form);
-};
+		$('#animal-form').append(form);
+	};
 
-function ajaxCall(){
+	function ajaxCall(){
 
-	$("#giphy-display").empty();
+		$("#giphy-display").empty();
 
-	let url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + animalSelect;
+		let url = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + animalSelect;
 
-	$.ajax({
-      url: url,
-      method: 'GET'
-    }).done(function(response) {
-    	response = response.data;
-    	
+		$.ajax({
+	      url: url,
+	      method: 'GET'
+	    }).done(function(response) {
+	    	response = response.data;
+	    	
 
-    	for (let i = 0; i < response.length; i++) {
+	    	for (let i = 0; i < response.length; i++) {
 
-    		let rating = $("<p>");
+	    		let rating = $("<p>");
 
-    		rating.text("Rating: "+response[i].rating);
+	    		rating.text("Rating: "+response[i].rating);
 
-    		let gif = $("<img>");
+	    		let gif = $("<img>");
 
-    		gif.addClass("img-fluid imgClass");
+	    		gif.addClass("img-fluid imgClass");
 
-    		gif.attr("src", response[i].images.fixed_height_still.url);
+	    		gif.attr("src", response[i].images.fixed_height_still.url);
 
-    		gif.attr("data-still", response[i].images.fixed_height_still.url);
+	    		gif.attr("data-still", response[i].images.fixed_height_still.url);
 
-    		gif.attr("data-animate", response[i].images.fixed_height.url);
+	    		gif.attr("data-animate", response[i].images.fixed_height.url);
 
-    		gif.attr("data-state", "still");
+	    		gif.attr("data-state", "still");
 
-    		gif.addClass("gif");
+	    		gif.addClass("gif");
 
-    		let div = $("<div>");
+	    		let div = $("<div>");
 
-    		div.addClass("gifdiv")
+	    		div.addClass("gifdiv")
 
-    		div.append(rating, gif);
+	    		div.append(rating, gif);
 
-    		$("#giphy-display").append(div);
-    	}
+	    		$("#giphy-display").append(div);
+	    	}
 
-    	$(".gif").on("click", function() {
-			// The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-			var state = $(this).attr("data-state");
-			// If the clicked image's state is still, update its src attribute to what its data-animate value is.
-			// Then, set the image's data-state to animate
-			// Else set src to the data-still value
-			if (state === "still") {
-			$(this).attr("src", $(this).attr("data-animate"));
-			$(this).attr("data-state", "animate");
-			} else {
-			$(this).attr("src", $(this).attr("data-still"));
-			$(this).attr("data-state", "still");
-	      	}
-    	});
-    });
-};
+	    	$(".gif").on("click", function() {
+				// The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+				var state = $(this).attr("data-state");
+				// If the clicked image's state is still, update its src attribute to what its data-animate value is.
+				// Then, set the image's data-state to animate
+				// Else set src to the data-still value
+				if (state === "still") {
+				$(this).attr("src", $(this).attr("data-animate"));
+				$(this).attr("data-state", "animate");
+				} else {
+				$(this).attr("src", $(this).attr("data-still"));
+				$(this).attr("data-state", "still");
+		      	}
+	    	});
 
-//display button for each index of the array
-$("document").ready(function(event){
-	
+	    	
+	    });
+	};
+
+
 	fillButtons();
 	addForm();
 
-	$(".btnAnimal").on("click", function(){
-	animalSelect = $(this).text();
-	ajaxCall();
 	
-	});
 
 	$("#submit").on("click",function(event){
 		let newAnimal = $("#animalValue").val().trim();
 		$("#animalValue").val("");
 		animalNames.push(newAnimal);
 		fillButtons();
-
 	});
-
-	
 });
 
 
